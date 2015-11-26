649f5d10f5f751f040220b1c6b4419db8cbc4ede
Christian Parker <cp@cjparker.us>
2015-11-26 11:56:11 -0700

    basically working

--- temp.js
+++ temp.js
@@ -4,9 +4,71 @@
 
 var tempsensor = require('ds18x20');
 var _ = require('underscore');
+var Q = require('q');
+var minimist = require('minimist');
+var argv = require('minimist')(process.argv.slice(2));
 
-tempsensor.isDriverLoaded(function(err,isLoaded) {
-  console.log(isLoaded);
-});
+module.exports = (function() {
+  // promise based versions
+
+  var isDriverLoaded = Q.nbind(tempsensor.isDriverLoaded, tempsensor);
+  var loadDriver = Q.nbind(tempsensor.loadDriver, tempsensor);
+  var listSensors = Q.nbind(tempsensor.list, tempsensor);
+  var getAll = Q.nbind(tempsensor.getAll, tempsensor);
+
+  var ensureDriverLoaded = function() {
+    return isDriverLoaded()
+      .then(function(isLoaded) {
+        console.log('driver is loaded', isLoaded);
+        if (isLoaded) {
+          return Q('driver is loaded');
+        } else {
+          return loadDriver();
+        }
+      })
+      .fail(function(x) {
+        console.log('driver is not loaded',x,'attempting to load now');
+        return loadDriver();
+      })
+      .fail(function(err) {
+        console.log('error loading driver', err);
+      })
+  };
+
+  var getTempF = function() {
+    var firstSensor;
+
+    return ensureDriverLoaded()
+      .then(function() {
+        return listSensors();
+      })
+      .then(function(sensors) {
+        console.log('got sensors', sensors);
+        if (sensors.length > 0) {
+          firstSensor = sensors[0];
+          console.log('using first sensor listed',firstSensor);
+          return getAll();
+        }
+      })
+      .then(function(readings) {
+        var celcius = readings[firstSensor];
+        return Q(celcius * 1.8 + 32);
+      });
+    
+  };
+
+  // if --test, then read a value from the sensor and print to console
+  if (argv.test) {
+    getTempF()
+    .then(function(result) {
+      console.log('result',result);
+    });
+  }
+
+  return {
+    getTempF : getTempF
+  }
+
+})();
 
 
