1e647d28b9b8186cf53d916a913bf3ca15252c3b
Christian Parker <christian.parker@emc.com>
2015-11-28 15:25:56 -0700

    cleaned up a little

--- re-encode-vids.js
+++ re-encode-vids.js
@@ -8,37 +8,50 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 var _ = require('underscore');
 
-module.exports = (function() {
+module.exports = (function () {
 
   var appDir = '/opt/house-monitor';
   var newVidsDir = '/new-vids';
   var liveVidsDir = '/live-vids';
 
   var readdir = Q.nfbind(fs.readdir);
-  var handleIO = function (error, stdout, stderr) { 
+  var unlink = Q.nfbind(fs.unlink);
+
+  var handleIO = function (error, stdout, stderr) {
     if (error) {
       console.log(error);
     }
     sys.puts(stdout);
     sys.puts(stderr);
-  }
+  };
 
+  // look for new vids uploaded
   readdir(appDir + newVidsDir)
-    .then(function(vids) {
-      console.log('',vids.length,'new videos to re-encode');
-      console.log('vids',vids);
+    .then(function (vids) {
+      console.log('', vids.length, 'new videos to re-encode');
+      console.log('vids', vids);
       var encodeCommand = 'avconv -i $1 -vcodec libx264 -vprofile high -preset slow -b:v 1000k -maxrate 1000k -bufsize 200k -r 4 $2';
 
-      _.each(vids, function(vid) {
+      _.each(vids, function (vid) {
+
         var command = encodeCommand
           .replace('$1', appDir + newVidsDir + '/' + vid)
-          .replace('$2', appDir + liveVidsDir + '/' + vid.replace(/^(.*?)\.avi$/,'$1.mp4') );
-        console.log('running\n',command);
-      exec(command,handleIO);
+          .replace('$2', appDir + liveVidsDir + '/' + vid.replace(/^(.*?)\.avi$/, '$1.mp4'));
+
+        console.log('running\n', command);
+
+        try {
+          exec(command, handleIO);
+        } catch (err) {
+          console.log('caught error encoding.  SKIPPING.')
+        }
+
+        // remove the old vid
+        unlink(appDir + newVidsDir + '/' + vid);
       })
     })
-    .catch(function(err) {
-      console.log('caught error',err);
+    .catch(function (err) {
+      console.log('caught error', err);
     })
 
 })();
