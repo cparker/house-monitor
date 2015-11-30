69d4998c9bce46166b13fddaf483205fc23544d2
Christian Parker <cp@cjparker.us>
2015-11-29 18:31:29 -0700

    uploader

--- upload-new-vids.js
+++ upload-new-vids.js
@@ -13,8 +13,12 @@ module.exports = (function () {
   var readdir = Q.nfbind(fs.readdir);
 
   //var motionContentDir = '/media/iomega/motion';
-  var motionContentDir = 'src';
+  var motionContentDir = '/media/iomega/motion';
   var lastCheckFile = 'lastCheck.json';
+  var ec2key = '/media/iomega/bigopt/house-monitor/ec2-march-2014.pem';
+  var ec2UserAndHost = 'ubuntu@cjparker.us';
+  var movieUploadLocation = '/opt/house-monitor/new-vids/';
+  var picUploadLocation = '/opt/house-monitor/live-vids/';
 
   readdir(motionContentDir)
     .then(function (motionFiles) {
@@ -28,7 +32,7 @@ module.exports = (function () {
         }
       } catch (err) {
         console.log('looks like a first run ', err);
-        lastCheck = {date: new Date()}
+        lastCheck = {date: new Date(0)}
       }
       console.log('looking for files written since ', lastCheck);
 
@@ -46,15 +50,32 @@ module.exports = (function () {
       var newFiles = _.filter(motionFileStats, function (fileStat) {
         if (fileStat.stat.ctime > lastCheck.date) {
           console.log('found a new file ', fileStat.name);
-          return fileStat.name;
+          return true;
         }
       });
 
-      var uploadCommand = 'ls -la $1'.replace('$dir',motionContentDir);
+      var movieUploadCommand = '/usr/bin/scp -p -i $1 $2 $3:$4'
+        .replace('$1', ec2key)
+        .replace('$3', ec2UserAndHost)
+        .replace('$4', movieUploadLocation)
 
-      _.each(newFiles, function (fileToUpload) {
-        console.log('working ',fileToUpload.name);
-        exec(uploadCommand.replace('$1',fileToUpload.name), function(err,stdout,stderr) {
+      var picUploadCommand = '/usr/bin/scp -p -i $1 $2 $3:$4'
+        .replace('$1', ec2key)
+        .replace('$3', ec2UserAndHost)
+        .replace('$4', picUploadLocation)
+
+      var movies = _.filter(newFiles, function(f) {
+        return f.name.match('.avi'+'$')=='.avi';
+      })
+
+      var pics = _.filter(newFiles, function(f) {
+        return f.name.match('.jpg'+'$')=='.jpg';
+      })
+
+      // MOVIES
+      _.each(movies, function (fileToUpload) {
+        console.log('running ', movieUploadCommand.replace('$2',fileToUpload.name))
+        exec(movieUploadCommand.replace('$2',fileToUpload.name), function(err,stdout,stderr) {
           if (err) {
             console.log(err);
           }
@@ -62,13 +83,25 @@ module.exports = (function () {
           sys.puts(stderr);
 
         });
+      });
 
+      // PICS
+      _.each(pics, function (fileToUpload) {
+        console.log('running ', picUploadCommand.replace('$2',fileToUpload.name))
+        exec(picUploadCommand.replace('$2',fileToUpload.name), function(err,stdout,stderr) {
+          if (err) {
+            console.log(err);
+          }
+          sys.puts(stdout);
+          sys.puts(stderr);
+
+        });
       });
 
       fs.writeFileSync(lastCheckFile, JSON.stringify({date: new Date()}));
     })
     .catch(function (err) {
-      console.log(err);
+      console.log('caught ',err);
     })
 
 })();
