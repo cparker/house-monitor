671616f6143d8c7f5e37f4a3df9dd6743a807e3d
Christian Parker <christian.parker@emc.com>
2015-12-14 13:51:19 -0700

    basic line chart works!

--- src/temp-chart.ts
+++ src/temp-chart.ts
@@ -1,8 +1,9 @@
 import {Http, Response} from 'angular2/http'
-import {Component, View, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, View, CORE_DIRECTIVES,OnInit,AfterContentInit,AfterViewInit} from 'angular2/angular2';
 import {MockDataService} from './mock-data-service';
 import {EventService} from './event-service';
 
+declare var google;
 
 @Component({
   selector: 'temp-chart'
@@ -18,8 +19,40 @@ import {EventService} from './event-service';
 })
 
 
-export class TempChart {
+export class TempChart implements OnInit, AfterContentInit, AfterViewInit {
   constructor() {
     console.log('TempChart constructor');
   }
+
+  ngOnInit() {
+    console.log('ngOnInit got called');
+  }
+
+  ngAfterContentInit() {
+    console.log('after content init');
+  }
+
+  ngAfterViewInit() {
+    console.log('after view init');
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ]);
+      var options = {
+        title: 'Company Performance',
+        curveType: 'function',
+        legend : { position : 'bottom' }
+      };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
+
+    chart.draw(data,options);
+
+
+  }
+
+
 }
